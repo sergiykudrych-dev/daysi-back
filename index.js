2a2bbfa793bd927482fb089f1464a89b70fe18f0
sergiykudrych-dev <sergiy.kudrych24@gmail.com>
2024-08-05 21:52:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const router = require('./routes/index');
+const errormiddleware = require('./middlewares/error-middleware');
+const path = require('path');
+
+const PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    // origin: 'http://localhost:3000',
+    origin: 'https://daysi.netlify.app',
+  })
+);
+app.use(express.json({ extended: true }));
+app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use(cookieParser());
+app.use('/api', router);
+app.use(errormiddleware);
+
+const start = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL, {});
+
+    app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
